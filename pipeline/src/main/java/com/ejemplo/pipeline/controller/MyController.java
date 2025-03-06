f0ec44051c8855a7b46024d6239a2c2500e443f5
jfguevara16 <juan_fe_gue@hotmail.com>
2025-03-05 19:44:38 -0500

    Desarrollo guía

---
+++ pipeline/src/main/java/com/ejemplo/pipeline/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.ejemplo.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
+
